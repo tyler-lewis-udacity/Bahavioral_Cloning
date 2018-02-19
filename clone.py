de912cc74385af42d35c0fe2f251116c7a8c6c41
Tyler Lewis <tyler.lewis.udacity@gmail.com>
2018-02-18 21:13:53 -0600

    Updated to use NVIDIA model

--- clone.py
+++ clone.py
@@ -12,7 +12,7 @@ with open('./data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 
-for line in lines[1:]:
+for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
     current_path = './data/IMG/' + filename
@@ -25,13 +25,31 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Convolution2D, Cropping2D
 
+# # Simple Architecture:
+# model = Sequential()
+# model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+# model.add(Flatten())
+# model.add(Dense(1))
+
+# NVIDIA Architecture:
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="elu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="elu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="elu"))
+model.add(Convolution2D(64, 3, 3, activation="elu"))
+model.add(Convolution2D(64, 3, 3, activation="elu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
+
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
 
 model.save('model.h5')
